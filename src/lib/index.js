c5cbfa4a59c6be1633076d4232dd5028318e351a
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2024-09-19 15:00:58 -0400

    Create all components needed

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,5 @@
-// place files you want to import through the `$lib` alias in this folder.
+export { default as Nav } from './components/Nav.svelte';
+export { default as LastUpdated } from './components/LastUpdated.svelte';
+export { default as Projects } from './components/Projects.svelte';
+export { default as Project } from './components/Project.svelte';
+export { default as Services } from './components/Services.svelte';
