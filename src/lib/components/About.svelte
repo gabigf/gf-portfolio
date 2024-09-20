62d55860162cd931a9fe179f9d1da9b1a2cedda7
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2024-09-19 21:13:34 -0400

    Add about section

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -1,1 +1,23 @@
-<p>about</p>
+<style>
+.core-container {
+  margin-bottom: 2rem
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
+
+
+<div class="core-container">
+  <p>Core:</p>
+  <ul>
+    <li>Front end Development</li>
+    <li>Framework-Agnostic Solutions</li>
+    <li>Preference for Vue</li>
+    <li>Component-Driven development</li>
+    <li>Collaborative and Open to Learning</li>
+  </ul>
+</div>
