f50ccb78ac49e6dead13d7208746aa32c622f577
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2024-09-27 02:55:27 -0400

    Add vercel speed insights

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script>
   import '../app.css';
   import {  MainLayout, Logo } from '$lib'
+  import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+  injectSpeedInsights();
 </script>
 
 <Logo />
